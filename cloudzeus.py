45cda75c99d7417824a1df078751953b58e663ab
BALAVIGNESH  (PYTHONEER) <36058020+BALAVIGNESHDOSTRIX@users.noreply.github.com>
2020-06-12 14:48:37 +0530

    python file added

---
+++ cloudzeus.py
@@ -0,0 +1,23 @@
+import eel 
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import base64
+import io 
+import urllib
+
+def plot_wordcloud(data):
+    wc = WordCloud(background_color='black')
+    wc.generate(data)
+    return wc.to_image()
+
+@eel.expose
+def str2base64(n:str):
+    pic_IObytes = io.BytesIO()
+    plot_wordcloud(data=n).save(pic_IObytes, format='PNG')
+    return 'data:image/png;base64,{}'.format(base64.b64encode(pic_IObytes.getvalue()).decode())
+
+
+eel.init('web')
+eel.start('index.html', size=(1000,600))
